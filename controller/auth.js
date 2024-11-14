7afd73d12e0e6c861a6200f56a73a94287a3b679
dsabljic <domagoj.sabljicc@gmail.com>
2024-11-14 20:56:26 +0100

    add signup request handler

---
+++ controller/auth.js
@@ -0,0 +1,33 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../model/user");
+const { throwError } = require("../util/error");
+
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessage = errors
+      .array()
+      .map((err) => err.msg)
+      .join(", ");
+    return throwError(400, errorMessage, next);
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  try {
+    const hashedPassword = bcrypt.hash(password);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (err) {
+    throwError(500, "Failed to create a new user", next);
+  }
+};
