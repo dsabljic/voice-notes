4cde5af8f6c2fce8e041a93362d7d7087865a7c2
dsabljic <domagoj.sabljicc@gmail.com>
2024-11-17 23:56:08 +0100

    transition to sqlite from mysql

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("voice-notes-ai", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./voice-notes-ai.sqlite",
 });
 
 module.exports = sequelize;
