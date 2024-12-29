b77475e98844463f38f2de9c58f229584be5c551
dsabljic <domagoj.sabljicc@gmail.com>
2024-12-29 21:28:33 +0100

    fix foreign key constraints

--- util/database.js
+++ util/database.js
@@ -3,6 +3,21 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./voice-notes-ai.sqlite",
+  define: {
+    foreignKeyConstraints: true,
+  },
+  dialectOptions: {
+    foreign_keys: true,
+  },
 });
+
+sequelize
+  .query("PRAGMA foreign_keys = ON")
+  .then(() => {
+    console.log("Foreign key constraints enabled.");
+  })
+  .catch(() => {
+    console.error("Error enabling foreign key constraints:", err);
+  });
 
 module.exports = sequelize;
